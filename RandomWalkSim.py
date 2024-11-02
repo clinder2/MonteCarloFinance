453692b87c04005b107e0ef1b64873542fce2540
clinder2 <clinder9@gatech.edu>
2024-11-02 19:17:45 -0400

    Update RandomWalkSim.py

--- RandomWalkSim.py
+++ RandomWalkSim.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 from random import randint
+import matplotlib.pyplot as plt
 
 days = 90
 dt = 1/365 # time in years
@@ -9,13 +10,20 @@ S0 = 20
 X = 25 # strike price
 r = .031 # risk free rate
 sigma = .6 # volitility
-trials = 2
+trials = 1
 value = 0
-for i in range(1, trials):
+prices = [trials]
+for i in range(0, trials):
     n = [np.random.normal(.5, .5, 1) for p in range(0, days)]
     S = S0
     for j in range(0, days):
         dS = S*(r*dt + sigma*math.sqrt(dt)*n[j])
         S = S + dS
+        print(dS)
     value = value + max(S-X, 0)
-print(value)
+    prices.append(value)
+    print(str(i) + ", " + str(S))
+print(value/trials)
+print(math.exp(-1*r*T) * value/trials)
+plt.hist(prices)
+plt.show()
